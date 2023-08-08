e80d086c3b760136c44908e60ea03aa1f0b3326b
James Baxter <39841697+J-Baxter@users.noreply.github.com>
2023-08-08 11:57:39 +0100

    Update rstudio_installer.sh
    minor refactoring

--- rstudio_installer.sh
+++ rstudio_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Update all packages and install dependencies
-sudo apt-get install gdebi-core whois libcurl4-openssl-dev libxml2-dev libfontconfig1-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev cmake
+sudo apt-get install gdebi-core whois 
 sudo apt-get update
 sudo apt-get upgrade
 
@@ -10,6 +10,7 @@ wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sud
 echo "deb [signed-by=/usr/share/keyrings/r-project.gpg] https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" | sudo tee -a /etc/apt/sources.list.d/r-project.list
 sudo apt update
 sudo apt install --no-install-recommends r-base r-base-dev
+sudo apt install libcurl4-openssl-dev libxml2-dev libfontconfig1-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev cmake
 
 # Install R Studio
 wget https://download2.rstudio.org/server/jammy/amd64/rstudio-server-2023.06.1-524-amd64.deb #change to get latest version
@@ -17,7 +18,7 @@ sudo gdebi rstudio-server-2023.06.1-524-amd64.deb
 sudo /bin/bash -c “echo ‘www-address=127.0.0.1’ >> /etc/rstudio/rserver.conf”
 sudo rstudio-server restart
 
-echo 'Please enter a username (\this will be your login for RStudio):'
+echo 'Please enter a username (this will be your login for RStudio):'
 read username
 
 echo 'Please enter a password (this will be your password for RStudio):'
