5576be20453bfd6e2adaf621223bf1bcebb76dfc
James Baxter <39841697+J-Baxter@users.noreply.github.com>
2024-01-21 21:33:18 +0000

    Update usefuloneliners.sh
    grep ">" file.fasta | wc -l

--- usefuloneliners.sh
+++ usefuloneliners.sh
@@ -5,4 +5,7 @@ ls -1 | wc -l
 # print filenames that are not duplicated
 ls -1 | sed 's/\.RELAX\.json$//' | sort | uniq -c | awk '$1 == 1{print $2}'
 
-#
+# number of sequences in a .fasta file
+grep ">" file.fasta | wc -l
+# and in a loop...
+for FILE in *.fasta; do grep ">" $FILE | wc -l; done
